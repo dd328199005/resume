a693fef2127bf51927f11d6fba918a6e21b916e0
daskys <az331127@163.com>
2018-02-04 00:00:13 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 let webpack = require('webpack')
 let path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
     entry: path.join(__dirname,'./src/js/index.js'),
     output: {
@@ -33,34 +33,35 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    }, {
-                        loader: "css-loader",
-                        
-                    },
-                    {
-                        loader: "postcss-loader", 
-                    },
-                    {
-                        loader: "sass-loader"
-                    }
-                ],
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                minimize: true
+                            }
+                        },
+                        "postcss-loader",
+                        "sass-loader"
+                    ]
+                }),
                 exclude: /node_modules/
             },
             {
                 test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    }, {
-                        loader: "css-loader"
-                    },
-                    {
-                        loader: "postcss-loader",
-                    }
-                ],
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                minimize: true
+                            }
+                        },
+                        "postcss-loader",
+                    ]
+                }),
                 exclude: /node_modules/
             }
         ]
@@ -73,5 +74,6 @@ module.exports = {
             template: __dirname + "/index.html"
         }),
         new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin("style.css"), //分离css
     ]
 }
